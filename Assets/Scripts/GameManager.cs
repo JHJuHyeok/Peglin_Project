1bf0b7b6ebb4dc27fec74569384b553a60251378
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-10-30 11:25:34 +0900

    SampleScene 이름 변경, PlayerManager 스크립트 생성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
-    
+    [Header("플레이어 정보")]
+    public int MaxHP;           // 최대 체력
+    public int currentHP;       // 현재 체력
+    public int currentCoin;     // 소지금
+    // 현재 지니고 있는 오브 리스트
+    // 지니고 있는 유물 아이템 리스트
+    // 현재 위치
+
+
 }
